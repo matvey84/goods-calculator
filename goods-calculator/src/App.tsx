ad24980353ead45587643cfd869337b27ab02a67
matvey84 <matvey8407@mail.ru>
2023-03-30 21:04:23 +0300

    feat: add all component into App

--- goods-calculator/src/App.tsx
+++ goods-calculator/src/App.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
+import OrderForm from './components/formField/OrderForm';
+import ResultField from './components/resultField/ResultField';
+import { configBD, matterialBD } from './DATA/data';
+import { getConfigAction, getDataAction } from './redux/formSlice';
+import { useAppDispatch } from './redux/hooks';
 
 function App() {
-  return <div className="App"></div>;
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(getDataAction(matterialBD));
+    dispatch(getConfigAction(configBD));
+  });
+  return (
+    <div className="App">
+      <section className="app-container">
+        <OrderForm />
+        <ResultField />
+      </section>
+    </div>
+  );
 }
 
 export default App;
